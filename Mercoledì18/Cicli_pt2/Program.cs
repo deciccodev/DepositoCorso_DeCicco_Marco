80d1c17e8d68a6ead1b26617e5936129ba20104d
deciccodev <deciccodev@gmail.com>
2026-02-18 12:59:28 +0100

    Add files via upload

---
+++ Mercoledì18/Cicli_pt2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Cicli_pt2
+{
+    public static void Main(string[] args)
+    {
+        bool continua = true;
+        int cont = 0;
+        do
+        {
+            Console.WriteLine("Ciclo in esecuzione");            
+            cont++;
+            if(cont >= 3)
+            {
+                continua = false;
+            }
+        }
+        while (continua);
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ Mercoledì18/EsercizioComplessivo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class EsercizioComplessivo
+{
+    public static void Main(string[] args)
+    {
+        string login = "", nomeUtente = "";
+        int password = 0;
+        bool continua = true;
+
+        Console.WriteLine("Hai già un account? si/no: ");
+        login = Console.ReadLine();
+
+        while (continua)
+        {
+            switch (login)
+            {
+                case "no":
+                Console.WriteLine("Inserisci il tuo nome utente: ");
+                nomeUtente = Console.ReadLine();
+                Console.WriteLine("Inserisci la tua password: ");
+                password = int.Parse(Console.ReadLine());
+                break;
+
+                default:
+                    Console.WriteLine("Inserisci il tuo nome utente: ");
+                    string tmp = Console.ReadLine();
+                    if(tmp != nomeUtente)
+                    {
+                        Console.WriteLine("Nome utente non valido!");
+                        break;
+                    }
+                    Console.WriteLine("Inserisci la tua password: ");
+                    int tmp_password = int.Parse(Console.ReadLine());
+                    if(tmp_password != password)
+                    {
+                        Console.WriteLine("Password non valida!");
+                        break;
+                    }
+                    else
+                    {
+                        do
+                        {    
+                            Console.WriteLine("Inserisci da dove far partire il conto alla rovescia: ");
+                            int countdown = int.Parse(Console.ReadLine());
+                            for (int i = countdown; i >= 0; i--)
+                            {
+                                Console.WriteLine($"conto alla rovescia: {i}");
+                            }
+
+                            Console.WriteLine("Vuoi continuare? (true/false): ");
+                            continua = bool.Parse(Console.ReadLine());
+                        } while (continua);
+                    }                
+                    break;
+            
+            }
+        
+        }
+
+    }
+}
